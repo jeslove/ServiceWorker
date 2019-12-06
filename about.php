9873e70ff124120a8b28e98d539dfa3f79810338
jeslove <jerryjeslove@gmail.com>
2019-12-06 10:14:07 +0000

    Create about.php

---
+++ about.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="manifest" href="manifest.json">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <title>About</title>
+</head>
+
+<body>
+
+    <div class="navbar">
+        <a class="navbar-brand" href="#">Title</a>
+        <ul class="nav navbar-nav">
+            <li class="active">
+                <a href="index.php">Home</a>
+            </li>
+            <li>
+                <a href="about.php">About</a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="container">
+    
+        <div class="row">
+    
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <h1 class="text-center text-danger">ABOUT PAGE</h1>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta animi vitae architecto beatae, unde
+                    reprehenderit
+                    impedit, voluptatibus esse ratione at enim explicabo temporibus inventore corporis veniam sed harum
+                    fugit
+                    blanditiis!
+                </p>
+    
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore dolorum est minima dolores
+                    voluptatibus,
+                    dignissimos tenetur sed placeat dolor vel eligendi, quae dicta commodi rem libero, facere vitae?
+                    Laboriosam,
+                    corrupti?
+                </p>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, obcaecati asperiores aspernatur mollitia officiis beatae error nam possimus maxime cumque veniam dolores nisi facere sit illum quis expedita sed nesciunt!
+                </p>
+
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque est ratione ea aperiam eum animi similique iste, ducimus molestias id delectus nobis consectetur omnis, eius vitae a tempore? Quia, cum.
+                </p>
+            </div>
+    
+        </div>
+    
+    </div>
+<script src="main.js"></script>
+</body>
+
+</html>
